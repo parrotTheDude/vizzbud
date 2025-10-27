45c15fb3532f7a2f20ba15b131da43affcb4bf7b
Jacob <program.acquaint533@passinbox.com>
2025-10-27 19:03:39 +1100

    Added the new class

--- resources/views/layouts/vizzbud.blade.php
+++ resources/views/layouts/vizzbud.blade.php
@@ -244,7 +244,7 @@
   <!-- Drawer -->
   <nav
     class="absolute right-0 top-0 z-20 h-full w-[86%] max-w-sm
-           bg-white/10 backdrop-blur-2xl border-l border-white/10 safe-top-ios
+           bg-white/10 backdrop-blur-2xl border-l border-white/10 safe-top-ios  
            ring-1 ring-white/10 shadow-2xl rounded-l-2xl overflow-y-auto
            will-change-transform"
     :class="open ? 'translate-x-0' : 'translate-x-full'"
